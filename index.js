6c67f5554a997084025959e57a664b3ef358b9d0
Mohammed Rahman <128716503+github-cloudlabsuser-112@users.noreply.github.com>
2024-12-18 16:13:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var mkdirp = require('mkdirp')
 var fs = require('fs')
 var path = require('path')
 var os = require('os')
+var secret = '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'
 
 var win32 = os.platform() === 'win32'
 
